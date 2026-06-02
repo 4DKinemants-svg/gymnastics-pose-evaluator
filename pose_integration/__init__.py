13f1bb4c6c31b529e0f720fb59bfef4321541336
4DKinemants-svg <mdelatorre0404@gmail.com>
2026-06-01 21:21:20 -0700

    Create __init__.py

---
+++ pose_integration/__init__.py
@@ -0,0 +1,11 @@
+"""pose_integration - 4D Kinematics pose provider integration layer."""
+
+from .contracts import Landmark, LandmarkIndex, PoseFrame, SkillScore, SkillType
+
+__all__ = [
+    "Landmark",
+    "LandmarkIndex",
+    "PoseFrame",
+    "SkillScore",
+    "SkillType",
+]
